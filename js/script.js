a3b3b9825d1ee7321448055999934f2d0fbe8507
RAHUL VR <iamrahulvr29@gmail.com>
2023-06-29 16:42:38 +0530

    js final code

--- js/script.js
+++ js/script.js
@@ -47,3 +47,43 @@ const changeDirection = e => {
 
 // click event on arrow button
 controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+
+const initGame = () => {
+  if(gameOver) return handleGameOver();
+  let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+  
+  if(snakeX === foodX && snakeY === foodY) {
+      updateFoodPosition();
+      snakeBody.push([foodY, foodX]); 
+      score++; 
+      highScore = score >= highScore ? score : highScore;
+      localStorage.setItem("high-score", highScore);
+      scoreElement.innerText = `Score: ${score}`;
+      highScoreElement.innerText = `High Score: ${highScore}`;
+  }
+  snakeX += velocityX;
+  snakeY += velocityY;
+  
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+      snakeBody[i] = snakeBody[i - 1];
+  }
+  snakeBody[0] = [snakeX, snakeY]; 
+
+  if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+      return gameOver = true;
+  }
+
+  for (let i = 0; i < snakeBody.length; i++) {
+      html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+
+      if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+          gameOver = true;
+      }
+  }
+  playBoard.innerHTML = html;
+}
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 250);
+document.addEventListener("keyup", changeDirection);
